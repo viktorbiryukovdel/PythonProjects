c00b65967515a0dc701843cd8ec601c67473c91f
viktorbiryukovdel <viktorbiryukovdel@gmail.com>
2026-07-22 22:33:50 +0900

    Implement basic guessing game functionality
    Fix input prompt and variable name for random number generation.

---
+++ GuessingGame.py
@@ -0,0 +1,17 @@
+import random
+
+print("Set the range of numbers in which you would like to guess:)
+number = int(input())
+random_number = random.randint(1, range_num)
+
+print("Now, guess the number between 1 and", number)
+guess = 0
+while guess != random_number:
+  print("Guess the number now:")
+  guess = int(input())
+  if guess > random_number:
+    print("Your number is too high!")
+  elif guess < random_number:
+    print("Your number is too low")
+  else:
+    print("You are spot on!")
